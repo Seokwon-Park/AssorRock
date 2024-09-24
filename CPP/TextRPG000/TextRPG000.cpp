c0d681632877c4a8c961b7e8ce55d4d49a3534f7
Seokwon-Park <rty3394@naver.com>
2024-09-24 16:47:12 +0900

    TextRPG 선공후공

--- CPP/TextRPG000/TextRPG000.cpp
+++ CPP/TextRPG000/TextRPG000.cpp
@@ -1,15 +1,18 @@
 ﻿#include <iostream>
 #include <conio.h>
+#include <random>
 
 const int LINECOUNT = 50;
 const int NAMELEN = 20;
 // Player
 int PlayerAtt = 10;
 int PlayerHp = 0;
+int PlayerSpeed = 10;
 char PlayerName[NAMELEN] = "NONE";
 
 int MonsterAtt = 10;
 int MonsterHp = 0;
+int MonsterSpeed = 10;
 char MonsterName[NAMELEN] = "NONE";
 
 void StrCopy(char* _Arr, int _BufferSize, const char* const _Name)
@@ -26,17 +29,19 @@ void StrCopy(char* _Arr, int _BufferSize, const char* const _Name)
 	}
 }
 
-void CreatePlayer(const char* const _Ptr, int _Att, int _Hp)
+void CreatePlayer(const char* const _Ptr, int _Att, int _Hp, int _Speed)
 {
 	StrCopy(PlayerName, NAMELEN, _Ptr);
 	PlayerAtt = _Att;
 	PlayerHp = _Hp;
+	PlayerSpeed = _Speed;
 }
-void CreateMonster(const char* const _Ptr, int _Att, int _Hp)
+void CreateMonster(const char* const _Ptr, int _Att, int _Hp, int _Speed)
 {
 	StrCopy(MonsterName, NAMELEN, _Ptr);
 	MonsterAtt = _Att;
 	MonsterHp = _Hp;
+	PlayerSpeed = _Speed;
 }
 
 void StatusRender(const char* _Name, int _Att, int _Hp)
@@ -77,40 +82,96 @@ void TakeDamage(int& _DefHp, int _Att)
 	_DefHp -= _Att;
 }
 
+void SpeedCheckRender(const char* const _Name)
+{
+	printf_s("%s 의 선공입니다\n", _Name);
+}
+
 void Damage(const char* const _AttName, const char* const _DefName, int& _DefHp, int _Att)
 {
 	TakeDamage(_DefHp, _Att);
 	DamageRender(_AttName, _DefName, _Att);
 }
 
+int GenSpeed(std::mt19937& gen, std::uniform_int_distribution<int>& dis)
+{
+	return dis(gen);
+}
+
 int main()
 {
-	CreatePlayer("TestPlayer", 10,100 );
-	CreateMonster("Orc", 10,50 );
+	CreatePlayer("TestPlayer", 10, 100, 20);
+	CreateMonster("Orc", 10, 50, 10);
+
+	srand(time(0));
+	std::mt19937 gen;
+	std::uniform_int_distribution<int> player(0, PlayerSpeed);
+	std::uniform_int_distribution<int> monster(0, MonsterSpeed);
 
 
 	while (true)
 	{
 		system("cls");
-		char Input= ' ';
+		char Input = ' ';
 
 		PlayerStatusRender();
 		MonsterStatusRender();
 
+		//int RandomPlayerSpeed = rand() % PlayerSpeed;
+		//int RandomMonsterSpeed = rand() % MonsterSpeed;
+		int RandomPlayerSpeed = GenSpeed(gen, player);
+		int RandomMonsterSpeed = GenSpeed(gen, monster);
+		bool isPlayerFirst = RandomPlayerSpeed > RandomMonsterSpeed;
+
 		Input = _getch();
 		system("cls");
-		TakeDamage(MonsterHp, PlayerAtt);
+		if (isPlayerFirst) // 플레이어 선공
+		{
+			TakeDamage(MonsterHp, PlayerAtt);
+		}
+		else
+		{
+			TakeDamage(PlayerHp, MonsterAtt);
+		}
 		PlayerStatusRender();
 		MonsterStatusRender();
-		DamageRender(PlayerName, MonsterName, PlayerAtt);
-		
+		if (isPlayerFirst)
+		{
+			SpeedCheckRender(PlayerName);
+			DamageRender(PlayerName, MonsterName, PlayerAtt);
+		}
+		else
+		{
+			SpeedCheckRender(MonsterName);
+			DamageRender(MonsterName, PlayerName, MonsterAtt);
+		}
+
+		// 공격 턴 교체
 		Input = _getch();
 		system("cls");
-		TakeDamage(PlayerHp, MonsterAtt );
+		if (isPlayerFirst) // 플레이어가 선공이었다면 몬스터 공격차례
+		{
+			TakeDamage(PlayerHp, MonsterAtt);
+		}
+		else
+		{
+			TakeDamage(MonsterHp, PlayerAtt);
+		}
 		PlayerStatusRender();
 		MonsterStatusRender();
-		DamageRender(PlayerName, MonsterName, PlayerAtt);
-		DamageRender(MonsterName, PlayerName, MonsterAtt);
+		if (isPlayerFirst)
+		{
+			SpeedCheckRender(PlayerName);
+			DamageRender(PlayerName, MonsterName, PlayerAtt);
+			DamageRender(MonsterName, PlayerName, MonsterAtt);
+		}
+		else
+		{
+			SpeedCheckRender(MonsterName);
+			DamageRender(MonsterName, PlayerName, MonsterAtt);
+			DamageRender(PlayerName, MonsterName, PlayerAtt);
+		}
+
 		Input = _getch();
 	}
 
